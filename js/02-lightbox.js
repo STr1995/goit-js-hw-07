c87cee0af21cfcdcc338d88e0b777423dd865422
Serhii <99601512+STr1995@users.noreply.github.com>
2022-08-26 23:02:05 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryCard = galleryItems
+  .map(({ preview, original, description}) => `
+<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}" 
+  title = "${description}"/>
+</a>`
+  )
+  .join("");
+
+galleryList.innerHTML = galleryCard;
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
